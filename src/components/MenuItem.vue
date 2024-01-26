d255b9437440375ebfacac7cd0469d544cbd2876
EdwinMalarge <edwin.malarge@outlook.com>
2024-01-26 11:10:45 +0100

    Dispatch scss

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,23 +1,16 @@
-<script>
-export default {
-    name: "MenuItem",
-    props: ["addToShoppingCart","image","inStock","quantity","name",]
-
-
-
-}
-</script>
-
 <template>
     <div class="menu-item">
         <img class="menu-item__image" :src="image.source" :alt="image.alt" />
         <div>
             <h3>{{ name }}</h3>
+            <p> Prix : {{ generatedPrice }}
+                <span v-if="onSale">(10% de réduction!)</span>
+            </p>
             <p v-if="inStock">En stock</p>
             <p v-else>En rupture de stock</p>
             <div>
-                <label for="add-item-quantity">Quantité : {{quantity}}</label>
-                <input id="add-item-quantity" :quantity="quantity" type="number" />
+                <label for="add-item-quantity">Quantité : </label>
+                <input id="add-item-quantity" v-model="quantity" type="number" />
                 <button @click="addToShoppingCart(quantity)">
                     Ajouter au panier d'achat
                 </button>
@@ -25,3 +18,53 @@ export default {
         </div>
     </div>
 </template>
+
+
+<script>
+export default {
+    name: "MenuItem",
+    props: ["addToShoppingCart", "image", "inStock", "price", "modelValue", "name",],
+
+    emits: ['update:modelValue'],
+    methods: {
+        changePageTitle(title) {
+            this.$emit('update:modelValue', title)
+        }
+    },
+
+    data() {
+        return {
+            onSale: false
+        }
+    },
+    computed: {
+        generatedPrice() {
+            if (this.onSale) {
+                return (this.price * 0.9).toFixed(2)
+            } else {
+                return this.price
+            }
+        }
+    },
+    beforeMount() {
+        const today = new Date().getDate()
+
+        if (today % 2 === 0) {
+            this.onSale = true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.menu-item {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    margin-bottom: 30px;
+
+    &__image {
+        max-width: 300px;
+    }
+}
+</style>
